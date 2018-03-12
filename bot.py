71329a5d1cd8c83ec0f22ff16d203ec5946708af
whmori17 <gabrielemori@outlook.com>
2018-03-12 20:45:00 +0100

    finally bot

---
+++ bot.py
@@ -0,0 +1,6 @@
+from classes.CalendarBot import CalendarBot
+
+token = '556106958:AAG1J8_WwMACUnABnKoFW971NvG964eiTOg'
+
+calendarBot = CalendarBot(token)
+calendarBot.run()
---
+++ classes/CalendarBot.py
@@ -0,0 +1,36 @@
+from pprint import pprint
+
+import telepot
+from telepot.loop import MessageLoop
+
+
+class CalendarBot:
+
+    def __init__(self, token):
+        self.token = token
+        self.bot = telepot.Bot(token)
+
+    def run(self):
+        MessageLoop(self.bot, self.handle).run_as_thread()
+
+    def handle(self, message):
+        methodName = message['text'].replace('/','')
+        chatId = message['chat']['id']
+        try:
+            getattr(self, methodName)(chatId)
+        except AttributeError:
+            raise NotImplementedError(
+                f"Class {self.__class__.__name__} does not implement {methodName}")
+
+    def fromtoevents(self, id):
+        self.bot.sendMessage(id, f"fromtoevents{id}")
+    def todayevents(self, id):
+        self.bot.sendMessage(id, 'todayevents')
+    def lastnevents(self, id):
+        self.bot.sendMessage(id, 'lastnevents')
+    def newevent(self, id):
+        self.bot.sendMessage(id, 'newevent')
+    def icsofevent(self, id):
+        self.bot.sendMessage(id, 'icsofevent')
+    def csvevents(self, id):
+        self.bot.sendMessage(id, 'csvevents')
