65d36ac4272c6d7b6b78d1bfd302a6c6d696cbd3
whmori17 <gabrielemori@outlook.com>
2018-03-14 20:30:13 +0000

    CGI test

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-import cgitb
-cgitb.enable()
-print("Content-Type: text/plain\r\n\r\n")
-print("Hello World!")
+#!/usr/bin/env python
+
+print("Content-type: text/html\n\n")
+print("<html>\n<body>")
+print("<div style=\"width: 100%; font-size: 40px; font-weight: bold; text-align: center;\">")
+print("Python Script Test Page")
+print("</div>\n</body>\n</html>")
